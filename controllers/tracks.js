095ac8141c670e2661f038dcfdd10d4813614cb4
AdriALV2005 <adrimon7221@gmail.com>
2024-10-03 12:00:23 -0500

    Arreglando cambios

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,22 +1,11 @@
 const { tracksModel } = require("../models");
 
-/**
- *
- * @param {*} req
- * @param {*} res
- */
-
 const getItems = async (req, res) => {
   const data = await tracksModel.find({});
   res.send({ data });
 };
 const getItem = (req, res) => {};
 
-/**
- *
- * @param {*} req
- * @param {*} res
- */
 const createItem = async (req, res) => {
   const { body } = req;
   console.log(body);
