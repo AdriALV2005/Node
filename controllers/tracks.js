a99b88d0e89a2aed117c4d4420532b6256d31b97
AdriALV2005 <adrimon7221@gmail.com>
2024-10-03 19:12:30 -0500

    terminando controllers

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -10,11 +10,20 @@ const getItems = async (req, res) => {
     handleHttpError(res, "Error_GetItems");
   }
 };
-const getItem = (req, res) => {};
+const getItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await tracksModel.findById(id);
+    res.send({ data });
+  } catch (err) {
+    handleHttpError(res, "Error_GetItem");
+  }
+};
 
 const createItem = async (req, res) => {
   try {
-    const body = matchedData(req)
+    const body = matchedData(req);
     const data = await tracksModel.create(body);
     res.send({ data });
   } catch (err) {
@@ -22,7 +31,25 @@ const createItem = async (req, res) => {
   }
 };
 
-const updateItem = (req, res) => {};
-const deleteItem = (req, res) => {};
+const updateItem = async (req, res) => {
+  try {
+    const {id, ...body} = matchedData(req);
+    const data = await tracksModel.findOneAndUpdate({ _id: id }, body);
+    res.send({ data });
+  } catch (err) {
+    console.log(err)
+    handleHttpError(res, "Error_UpdateItems");
+  }
+};
+const deleteItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await tracksModel.deleteOne({_id:id});
+    res.send({ data });
+  } catch (err) {
+    handleHttpError(res, "Error_DeleteItem");
+  }
+};
 
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { validatorCreateItem } = require("../validators/tracks");
-const { getItems, getItem, createItem } = require("../controllers/tracks");
+const {
+  validatorCreateItem,
+  validatorGetItem,
+} = require("../validators/tracks");
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/tracks");
 const router = express.Router();
-const customHeader = require("../middleware/customHeader")
+const customHeader = require("../middleware/customHeader");
 
 router.get("/", getItems);
+router.get("/:id", validatorGetItem, getItem);
 router.post("/", validatorCreateItem, createItem);
+router.put("/:id", validatorGetItem, validatorCreateItem, updateItem);
+router.delete("/:id",validatorGetItem,deleteItem)
 
 module.exports = router;
