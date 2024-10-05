6230af1cea708a83b5ca1fe42bcdd7c9f71a7d47
AdriALV2005 <adrimon7221@gmail.com>
2024-10-04 23:53:21 -0500

    terminando auth/login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,53 @@
 const { matchedData } = require("express-validator");
 const { usersModel } = require("../models");
 const { handleHttpError } = require("../utils/handleError");
-const { encrypt } = require("../utils/handlePassword");
+const { encrypt, compare } = require("../utils/handlePassword");
 const { tokenSign } = require("../utils/handleJwt");
 
-const createItem = async (req, res) => {
+const registerCtrl = async (req, res) => {
   try {
     req = matchedData(req);
     const password = await encrypt(req.password);
-    const body = {...req,password}
+    const body = { ...req, password };
     const dataUser = await usersModel.create(body);
-    dataUser.set('password',undefined,{strict:false})
+    dataUser.set("password", undefined, { strict: false });
     const data = {
-      token:await tokenSign(dataUser),
-      user:dataUser
-    }
+      token: await tokenSign(dataUser),
+      user: dataUser,
+    };
     res.send({ data });
   } catch (error) {
-    handleHttpError(res, "Error_CreateItems");
-    console.log(error)
+    handleHttpError(res, "Error_registerCtrl");
+    console.log(error);
   }
 };
 
-module.exports = { createItem };
+const loginCtrl = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const user = await usersModel
+      .findOne({ email: req.email })
+      .select("password name role email");
+    if (!user) {
+      handleHttpError(res, "Usuario no existente.", 404);
+      return;
+    }
+    const hashPassword = user.get("password");
+
+    const check = await compare(req.password, hashPassword);
+    if (!check) {
+      handleHttpError(res, "Contraseña invalida.", 401);
+      return;
+    }
+    const data = {
+      token: tokenSign(user),
+      user,
+    };
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, "Error_loginCtrl");
+    console.log(error);
+  }
+};
+
+module.exports = { registerCtrl, loginCtrl };
